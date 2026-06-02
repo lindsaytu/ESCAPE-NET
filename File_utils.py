29136dd8358cec4a4cd0ab319c9d9fc0d71ab58d
lindsaytu <lindsaytu2006@gmail.com>
2026-06-02 13:59:25 -0400

    Update File_utils.py

--- File_utils.py
+++ File_utils.py
@@ -6,7 +6,7 @@ Created on Mon Jan 4 2021
 """
 
 import sys
-sys.path.append('\\\\svm_uhn.uhn.ca\\NET\\NET2\\Zariffa\\Eugene\\NN Scripts') #change to current path of file_utils_main (note: all paths below are in windows format, change to mac or linux if needed)
+sys.path.append('\\\\svm_uhn.uhn.ca\\NET\\NET2\\Zariffa\\Eugene\\NN Scripts') #change to current path of file_utils_main (note: all paths below are in windows format, change to linux if needed)
 #sys.path.append('\\\\NET2\\Zariffa\\Eugene\\NN Scripts')
 
 import File_utils_main
