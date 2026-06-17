1306d1d441bb405bbad2912b0bc6a9e69850635a
lindsaytu <lindsaytu2006@gmail.com>
2026-06-17 12:05:34 -0400

    Update File_utils.py
    updated comments about file location for now

--- File_utils.py
+++ File_utils.py
@@ -6,10 +6,10 @@ Created on Mon Jan 4 2021
 """
 
 import sys
-sys.path.append('\\\\svm_uhn.uhn.ca\\NET\\NET2\\Zariffa\\Eugene\\NN Scripts') #change to current path of file_utils_main (note: all paths below are in windows format, change to linux if needed)
+sys.path.append('\\\\svm_uhn.uhn.ca\\NET\\NET2\\Zariffa\\Eugene\\NN Scripts') #save location (note: all paths below are in windows format, change to linux if needed)
 #sys.path.append('\\\\NET2\\Zariffa\\Eugene\\NN Scripts')
 
 import File_utils_main
 
 
-File_utils = File_utils_main.Utils('\\\\svm_uhn.uhn.ca\\NET\\NET2\\Zariffa\\Eugene\\NN Scripts\\Old Image Alignment\\') #change to current path of file_utils 
+File_utils = File_utils_main.Utils('\\\\svm_uhn.uhn.ca\\NET\\NET2\\Zariffa\\Eugene\\NN Scripts\\Old Image Alignment\\') #save location
