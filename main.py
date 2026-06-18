7a4c3d1ada21e3dde64cba643e875cb422d8838e
lindsaytu <lindsaytu2006@gmail.com>
2026-06-18 13:34:16 -0400

    created main file

---
+++ main.py
@@ -0,0 +1,11 @@
+
+#save location
+
+import sys
+sys.path.append('\\\\svm_uhn.uhn.ca\\NET\\NET2\\Zariffa\\Eugene\\NN Scripts') #save location (note: all paths below are in windows format, change to linux if needed)
+#sys.path.append('\\\\NET2\\Zariffa\\Eugene\\NN Scripts')
+
+import File_utils_main
+
+
+File_utils = File_utils_main.Utils('\\\\svm_uhn.uhn.ca\\NET\\NET2\\Zariffa\\Eugene\\NN Scripts\\Old Image Alignment\\') #save location
